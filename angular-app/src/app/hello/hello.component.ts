12145af074be691792b2d8a0ead0e65b1663b4a2
中井孝嘉 <nakaitakashiyoshimi@nakaitakashiyoshiminoMacBook-Pro.local>
2022-01-21 11:03:26 +0900

    20220121学習分

--- angular-app/src/app/hello/hello.component.ts
+++ angular-app/src/app/hello/hello.component.ts
@@ -9,14 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelloComponent implements OnInit {
 
+  // 変数の宣言
   title:string;
   message:string;
+  price:number;
+  now:Date;//日付
+  styleClass:string;//バインド用のクラス名
+
   // コンストラクタ
-  constructor() { }
+  constructor() { 
+    // 1sごとに処理実施
+    setInterval(()=>{this.now = new Date();},1000)
+
+
+  }
 // インターフェース
   ngOnInit() {
     this.title = 'Hellow-App';
     this.message = 'this is My First Component!!';
+    this.price=1123450;
+    this.styleClass="red";//赤にする
+  }
+
+  // あらかじめ用意した関数をhtml内の{{}}で表示
+  today(){
+    return new Date().toLocaleString();
+  }
+  today2(){
+
+    return this.now.toLocaleString();
   }
 
 }
