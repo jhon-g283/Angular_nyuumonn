38c04db87f4da2d2c959b32c7d79b104b9825e08
中井孝嘉 <nakaitakashiyoshimi@nakaitakashiyoshiminoMacBook-Pro.local>
2022-01-23 10:33:31 +0900

    20220123学習分

--- angular-app/src/app/hello/hello.component.ts
+++ angular-app/src/app/hello/hello.component.ts
@@ -20,16 +20,36 @@ import { Component, OnInit } from '@angular/core';
       <!-- ifでの分岐　Elseに該当するものがないのが特徴 -->
       <p *ngIf="visible" (click)="doClick2()" class="red">This is TRUE message.</p>
       <p *ngIf="visible == false" (click)="doClick2()" class="">This is FALSE message.</p>
+
+       <!-- loopでli表示 -->
       <ul>
         <li *ngFor="let item of data">{{item}}</li>
       </ul>
+
+      <!-- switch文・・なぜか動かない -->
+      <p>{{switch}}</p>
+      <p *ngIf="switch === 'one'">iii</p>
       <div [ngSwitch]="switch">
-          <p *ngSwitchCace="'one'">First paragraph!</p>
-          <p *ngSwitchCace="'two'">Second paragraph!</p>
+          <p *ngSwitchCace="one">First paragraph!</p>
+          <p *ngSwitchCace='two'>Second paragraph!</p>
           <p *ngSwitchCace="'three'">Third paragraph!</p>
-          <p *ngSwitchDefault=>Default paragraph</p>
+          <p *ngSwitchDefault>Default paragraph</p>
       </div>
-      <select #sel></select>
+      <select  #sel (change)="doSelect(sel.value)">
+        <option value="one">one</option>
+        <option>two</option>
+        <option>three</option>
+      </select>
+
+      <!--  Style バインディング -->
+      <p [style.font-size.pt] ="36" [style.color]="'#F00'" >{{message4}}:36</p>
+      <p [style.font-size.pt] ="28" [style.color]="'#F99'" >{{message4}}:36</p>
+      <p [style.font-size.pt] ="20" [style.color]="'#F20'" >{{message4}}:36</p>
+      
+      <p [ngClass]= "nowClass" >ngClass:{{message}}</p>
+      <input type="checkbox" #ck1 (change)="check(ck1.checked,ck2.checked,ck3.checked);">Thin
+      <input type="checkbox" #ck2 (change)="check(ck1.checked,ck2.checked,ck3.checked);">Large
+      <input type="checkbox" #ck3 (change)="check(ck1.checked,ck2.checked,ck3.checked);">Frame
       
     </div>
   
@@ -43,6 +63,7 @@ export class HelloComponent implements OnInit {
   message:string;
   message2:string;
   message3:string;
+  message4:string;
   price:number;
   now:Date;//日付
   styleClass:string;//バインド用のクラス名
@@ -50,6 +71,7 @@ export class HelloComponent implements OnInit {
   input:string;
   visible:boolean;
   data:string[];
+  switch:string;
 
   // コンストラクタ
   constructor() { 
@@ -66,16 +88,20 @@ export class HelloComponent implements OnInit {
   }
 // インターフェース
   ngOnInit() {
+
+    console.log("initialise");
     this.title = 'Hellow-App';
     this.message = 'this is My First Component!!';
     this.message2 = "thie is inline-template!"
     this.message3 = "--"
+    this.message4="Font Size";
     
     this.price=1123450;
     this.styleClass="red";//赤にする
     this.count=0;
     this.visible=false;
     this.data = ["1data","2data","3data"];
+    this.switch="one";
   }
 
   // あらかじめ用意した関数をhtml内の{{}}で表示
@@ -102,6 +128,19 @@ export class HelloComponent implements OnInit {
 
   doClick2(){
     this.visible = !this.visible;
+  }
+
+  doSelect(val:string){
+    this.switch=val;
+    console.log("change to "  + val);
+  }
+
+  // チェックボックスのクラス変更関数
+  check(c1,c2,c3){
+    this.nowClass.thin=c1;
+
+
+
   }
 
 }
