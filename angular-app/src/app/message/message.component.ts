cda6e4f30b6798fd2882b384c913891ae1e8f0c7
中井孝嘉 <nakaitakashiyoshimi@nakaitakashiyoshiminoMacBook-Pro.local>
2023-05-09 09:12:09 +0900

    20230508学習

--- angular-app/src/app/message/message.component.ts
+++ angular-app/src/app/message/message.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   // HTMLから参照するためのセレクタ
@@ -12,8 +12,56 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./message.component.css'],
 })
 export class MessageComponent implements OnInit {
-  @Input() content: string;
+  public _content: string[] = [];
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  // アクセサ
+  @Input()
+  set content(msg: string) {
+    this._content = msg.split(',');
+  }
+  get content() {
+    return this._content.join(',');
+  }
+
+  @Input() content2: string[];
+
+  @Input() content3: string[];
+
+  @Output() action = new EventEmitter<MouseEvent>(); //EventEmitter準備
+
+  doClick() {
+    this._content.pop();
+  }
+
+  // EventEmitterを使用して親コンポーネントから渡されたイベントを実行する
+  doAction(event: any) {
+    console.log('doAction event');
+    console.log(event);
+    this.action.emit(event);
+  }
+
+  push(item: string) {
+    console.log('子コンポ側のイベント関数をID指定で実行 : ' + item);
+
+    this.content2.push(item); //配列に追加
+  }
+
+  pushChildModuleFnc(item: string) {
+    console.log('親コンポのイベント関数を実行 : ' + item);
+    this.content3.push(item); //配列に追加
+  }
+
+  pop() {
+    console.log('子コンポ側のイベント関数をID指定で実行');
+    this.content2.pop();
+  }
+
+  popChildModuleFnc() {
+    console.log('親コンポのイベント関数を実行 : ');
+    this.content3.pop(); //配列に追加
+  }
 }
--- angular-app/src/app/parentofmessage/parentofmessage.component.ts
+++ angular-app/src/app/parentofmessage/parentofmessage.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MessageComponent } from '../message/message.component';
 
 // messageコンポーネントを読み込む用のコンポーネント
 @Component({
@@ -10,11 +11,62 @@ export class ParentofmessageComponent implements OnInit {
   // プロパティの設定
   title: string;
   message: string;
+  messageArray: string[];
+  messageArray2: string[];
+
+  lastTarget: any;
+  lastCalor: any;
+  input2: string;
+  // @ViewChild(MessageComponent)
+  @ViewChild('listNgModule') //デコレーションでインポートしてきた子コンポーネントの定義を行う、通常はインポートしたコンポーネント（MessageComponent）でいいが複数回コンポーネントをHTMLで読んでいるのでセレクタとしてIDを入れている。
+  private msgComponent: MessageComponent;
+
   constructor() {}
 
   // ngOnInit内でプロパティの値を設定
   ngOnInit(): void {
     this.title = 'Hello';
-    this.message = 'this is new Component';
+    // this.message = 'this is new Component';
+    // メッセージを配列にしてみる。
+    this.message = '1,2,3,4,5,6,7';
+    this.messageArray = ['A', 'B', 'C', 'D'];
+    this.messageArray2 = ['A', 'B', 'C', 'D'];
+    this.input2 = '';
+  }
+
+  doClick(event: any) {
+    console.log('event');
+    console.log(event);
+    if (this.lastTarget != null) {
+      this.lastTarget.style.color = this.lastCalor;
+      this.lastTarget.style.backgroundColor = 'white';
+    }
+
+    this.lastTarget = event.target; //クリックされた要素
+    this.lastCalor = event.target.style.color; //クリックされた要素の色
+    event.target.style.color = 'white';
+    event.target.style.backgroundColor = 'red';
+  }
+
+  ngAfterViewInit() {
+    console.log('-----');
+    console.log(this.msgComponent);
+  }
+
+  pushNgModule() {
+    // 子コンポーネントの方の関数を実行
+    console.log('ngModuleでイベント実施 : ' + this.input2);
+    if (this.input2 == '') {
+      alert('text is empty!!');
+      return;
+    }
+    console.log(this.msgComponent.pushChildModuleFnc);
+    this.msgComponent.pushChildModuleFnc(this.input2);
+    this.input2 = '';
+  }
+
+  popNgModule() {
+    console.log('ngModuleでイベント実施');
+    this.msgComponent.popChildModuleFnc();
   }
 }
